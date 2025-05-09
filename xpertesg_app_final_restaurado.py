93ff3aef4ad9046e65517693cfb80e77ddf32f5c
Luca Lando Simões <127948974+LucaLando@users.noreply.github.com>
2025-05-09 16:57:33 -0300

    Update xpertesg_app_final_restaurado.py

--- xpertesg_app_final_restaurado.py
+++ xpertesg_app_final_restaurado.py
@@ -170,7 +170,7 @@ if st.session_state.usuario:
     elif aba == "📈 Dashboards":
         st.subheader("📊 Análise ESG da Base de Clientes")
     
-        col1 = st.columns(1)
+        col1 = st.columns()
         with col1:
             fig1 = px.histogram(df, x="faixa_propensao", color="faixa_propensao",
                                 title="Distribuição por Faixa ESG",
