93878b3309b61dc53623f74dc24654d7a54614f1
Luca Lando Simões <127948974+LucaLando@users.noreply.github.com>
2025-05-30 14:57:37 -0300

    Update xpertesg_app_fixed.py

--- xpertesg_app_fixed.py
+++ xpertesg_app_fixed.py
@@ -131,7 +131,7 @@ if not st.session_state.usuario:
 
     with col1:
         # Logo principal
-        st.image("XPert1.PNG", use_container_width=True)
+        st.image("XPert2.PNG", use_container_width=True)
         st.markdown("## Login do Assessor")
 
         # Campo de entrada do usuário
